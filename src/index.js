40fe9889512a7f435a0ace501c9e58054ae4ba88
Antony Jones <aj@desirableobjects.co.uk>
2019-09-25 22:15:55 +0100

    fix loading as plain js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import CodeHighlight from './CodeHighlight.svelte'
 
 window.BEYONK = window.BEYONK || {}
-window.BEYONK.CodeHighlight = CodeHighlight
+window.BEYONK.CodeHighlight = function () {
+  const codeBlocks = document.querySelectorAll('[data-code-highlight]')
+  codeBlocks.forEach(b => {
+    const code = b.innerHTML
+    const locs = JSON.parse(b.dataset.locs)
+
+    b.innerHTML = ''
+
+    new CodeHighlight({
+      target: b,
+      props: {
+        code,
+        locs
+      }
+    })
+  })
+}
